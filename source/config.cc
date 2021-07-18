8bfd8c10342e451faab2bcd663869d9617f8d1a0
zhaohuabing <zhaohuabing@gmail.com>
2021-07-18 15:14:16 +0800

    update
    Signed-off-by: zhaohuabing <zhaohuabing@gmail.com>

--- source/config.cc
+++ source/config.cc
@@ -16,7 +16,7 @@ namespace MetaProtocolProxy {
 Network::FilterFactoryCb MetaProtocolProxyFilterConfigFactory::createFilterFactoryFromProtoTyped(
     const envoy::extensions::filters::network::meta_protocol_proxy::v1alpha::MetaProtocolProxy& proto_config,
     Server::Configuration::FactoryContext& context) {
-  //std::shared_ptr<Config> filter_config(std::make_shared<ConfigImpl>(proto_config, context));
+  std::shared_ptr<Config> filter_config(std::make_shared<ConfigImpl>(proto_config, context));
 
   return [filter_config, &context](Network::FilterManager& filter_manager) -> void {
     filter_manager.addReadFilter(std::make_shared<ConnectionManager>(
@@ -29,6 +29,66 @@ Network::FilterFactoryCb MetaProtocolProxyFilterConfigFactory::createFilterFacto
  */
 REGISTER_FACTORY(MetaProtocolProxyFilterConfigFactory,
                  Server::Configuration::NamedNetworkFilterConfigFactory);
+
+// class ConfigImpl.
+ConfigImpl::ConfigImpl(const MetaProctolProxyConfig& config,
+                       Server::Configuration::FactoryContext& context)
+    : context_(context);//, stats_prefix_(fmt::format("metaprotocol.{}.", config.stat_prefix())),
+      //stats_(DubboFilterStats::generateStats(stats_prefix_, context_.scope())),
+      //serialization_type_(
+      //    SerializationTypeMapper::lookupSerializationType(config.serialization_type())),
+      //protocol_type_(ProtocolTypeMapper::lookupProtocolType(config.protocol_type())) {
+  //auto type = RouteMatcherTypeMapper::lookupRouteMatcherType(config.protocol_type());
+  //route_matcher_ = Router::NamedRouteMatcherConfigFactory::getFactory(type).createRouteMatcher(
+      //config.route_config(), context);
+  /*if (config.dubbo_filters().empty()) {
+    ENVOY_LOG(debug, "using default router filter");
+
+    envoy::extensions::filters::network::dubbo_proxy::v3::DubboFilter router_config;
+    router_config.set_name(DubboFilters::DubboFilterNames::get().ROUTER);
+    registerFilter(router_config);
+  } else {
+    for (const auto& filter_config : config.dubbo_filters()) {
+      registerFilter(filter_config);
+    }
+  }*/
+}
+
+/*void ConfigImpl::createFilterChain(DubboFilters::FilterChainFactoryCallbacks& callbacks) {
+  for (const DubboFilters::FilterFactoryCb& factory : filter_factories_) {
+    factory(callbacks);
+  }
+}
+
+Router::RouteConstSharedPtr ConfigImpl::route(const MessageMetadata& metadata,
+                                              uint64_t random_value) const {
+  return route_matcher_->route(metadata, random_value);
+}
+
+ProtocolPtr ConfigImpl::createProtocol() {
+  return NamedProtocolConfigFactory::getFactory(protocol_type_).createProtocol(serialization_type_);
+}
+
+void ConfigImpl::registerFilter(const DubboFilterConfig& proto_config) {
+  const auto& string_name = proto_config.name();
+  ENVOY_LOG(debug, "    dubbo filter #{}", filter_factories_.size());
+  ENVOY_LOG(debug, "      name: {}", string_name);
+  ENVOY_LOG(debug, "    config: {}",
+            MessageUtil::getJsonStringFromMessageOrError(proto_config.config(), true));
+
+  auto& factory =
+      Envoy::Config::Utility::getAndCheckFactoryByName<DubboFilters::NamedDubboFilterConfigFactory>(
+          string_name);
+  ProtobufTypes::MessagePtr message = factory.createEmptyConfigProto();
+  Envoy::Config::Utility::translateOpaqueConfig(proto_config.config(),
+                                                ProtobufWkt::Struct::default_instance(),
+                                                context_.messageValidationVisitor(), *message);
+  DubboFilters::FilterFactoryCb callback =
+      factory.createFilterFactoryFromProto(*message, stats_prefix_, context_);
+
+  filter_factories_.push_back(callback);
+}*/
+
 } // namespace MetaProtocolProxy
 } // namespace NetworkFilters
 } // namespace Extensions
--- source/config.h
+++ source/config.h
@@ -29,6 +29,43 @@ private:
       Server::Configuration::FactoryContext& context) override;
 };
 
+class ConfigImpl : public Config,
+                   //public Router::Config,
+                   //public DubboFilters::FilterChainFactory,
+                   Logger::Loggable<Logger::Id::config> {
+public:
+  using MetaProctolProxyConfig = envoy::extensions::filters::network::meta_protocol_proxy::v1alpha::MetaProtocolProxy;
+  //using MetaProctolFilterConfig = envoy::extensions::filters::network::meta_protocol_proxy::v1alpha::MetaProtocolFilter;
+
+  ConfigImpl(const MetaProctolProxyConfig& config, Server::Configuration::FactoryContext& context);
+  ~ConfigImpl() override = default;
+
+  // DubboFilters::FilterChainFactory
+  //void createFilterChain(DubboFilters::FilterChainFactoryCallbacks& callbacks) override;
+
+  // Router::Config
+  //Router::RouteConstSharedPtr route(const MessageMetadata& metadata,
+  //                                  uint64_t random_value) const override;
+
+  // Config
+  //DubboFilterStats& stats() override { return stats_; }
+  //DubboFilters::FilterChainFactory& filterFactory() override { return *this; }
+  //Router::Config& routerConfig() override { return *this; }
+  //ProtocolPtr createProtocol() override;
+
+private:
+  //void registerFilter(const DubboFilterConfig& proto_config);
+
+  Server::Configuration::FactoryContext& context_;
+  const std::string stats_prefix_;
+  //DubboFilterStats stats_;
+  //const SerializationType serialization_type_;
+  //const ProtocolType protocol_type_;
+  //Router::RouteMatcherPtr route_matcher_;
+
+  //std::list<DubboFilters::FilterFactoryCb> filter_factories_;
+};
+
 } // namespace MetaProtocolProxy
 } // namespace NetworkFilters
 } // namespace Extensions
